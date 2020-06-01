683601f404909f05dc8de5c44a788299da47e68a
Bhargav Ram <bhargavramks@gmail.com>
2020-06-01 10:40:11 -0400

    Update Sum.java

--- src/effectivejava/chapter2/item6/Sum.java
+++ src/effectivejava/chapter2/item6/Sum.java
@@ -17,7 +17,10 @@ public class Sum {
 
         for (int i = 0; i < numSets; i++) {
             long start = System.nanoTime();
-            x += sum();
+            Long sum = 0L;
+            for (long i = 0; i <= Integer.MAX_VALUE; i++)
+                sum += i;
+            x += sum;
             long end = System.nanoTime();
             System.out.println((end - start) / 1_000_000. + " ms.");
         }
